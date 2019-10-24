51dc8212d77693ffe6b9fe6837410c9d892255a3
arnab1632 <56104436+arnab1632@users.noreply.github.com>
2019-10-24 14:44:45 +0600

    Add files via upload

---
+++ controllers/controllers/admin.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var userModel = require('./../models/user-model');
+var router = express.Router();
+
+router.get('/', function(request, response){
+	var username=request.cookies['username'];
+	userModel.getByusername(username, function(result){
+	response.render('admin/index');
+});
+});
+
+router.get('/addproducts', function(request, response){
+	response.render('admin/addproducts');
+});
+router.get('/deleteproducts', function(request, response){
+	response.render('admin/deleteproducts');
+});
+router.get('/deletecustomer', function(request, response){
+	response.render('admin/deletecustomer');
+});
+router.get('/addproducts', function(request, response){
+	response.render('admin/addproducts');
+});
+
+
+
+
+module.exports = router;
---
+++ controllers/controllers/login.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var userModel = require('./../models/user-model');
+var router = express.Router();
+
+router.get('/', function(request, response){
+	response.render('login/index');
+});
+
+router.post('/', function(request, response){
+	
+	var user = {
+		username: request.body.username,
+		password: request.body.password
+	};
+
+	userModel.validate(user, function(status){
+		if(status){
+			response.cookie('username', request.body.username);
+			response.redirect('/admin');
+			
+			
+		}else{
+			response.send('invalid username/password');		
+		}
+	});
+
+});
+
+module.exports = router;
+
+
