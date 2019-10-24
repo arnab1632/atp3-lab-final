4c074632861d29387227b2358b9f37a411d3e0d0
arnab1632 <56104436+arnab1632@users.noreply.github.com>
2019-10-24 10:30:40 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+//DECLARATION
+var express = require('express');
+//var bodyParser = require('body-parser');
+//var expSession = require('express-session');
+//var cookieParser = require('cookie-parser');
+var ejs = require('ejs');
+var login = require('./controllers/login');
+var register = require('./controllers/register');
+//var user = require('./controllers/user');
+//var logout = require('./controllers/logout');
+var app = express();
+
+
+//CONFIGURATION
+app.set('view engine', 'ejs');
+
+//MIDDLEWARE
+//app.use(bodyParser.urlencoded({extended:true}));
+//app.use(expSession({secret:'my top secret value', saveUninitialized:true, resave: false}));
+//app.use(cookieParser());
+app.use('/login', login);
+app.use('/register', register);
+//app.use('/user', user);
+//app.use('/logout', logout);
+
+
+
+//SERVER STARTUP
+app.listen(3000, function(){
+	console.log('server started at 3000...');
+});
